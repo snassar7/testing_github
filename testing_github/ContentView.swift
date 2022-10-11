2ab19cc0998bc3d66f556e42001e1737abfcf36a
Saif Nassar <snassar@andrew.cmu.edu>
2022-10-11 15:29:30 -0400

    first commit

---
+++ testing_github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testing_github
+//
+//  Created by Saif Nassar on 11/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testing_github/testing_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testing_githubApp.swift
+//  testing_github
+//
+//  Created by Saif Nassar on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct testing_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
